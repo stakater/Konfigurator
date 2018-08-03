d4ee4d07af935af077aae4c12a849d0eeb99fee2
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-08-03 18:54:51 +0500

    Add type meta and fill null reference exception

--- pkg/kube/configmap.go
+++ pkg/kube/configmap.go
@@ -1,6 +1,7 @@
 package kube
 
 import (
+	"github.com/stakater/Konfigurator/pkg/apis/konfigurator/v1alpha1"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -9,6 +10,10 @@ func CreateConfigMap(name string) *v1.ConfigMap {
 	return &v1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
+		},
+		TypeMeta: metav1.TypeMeta{
+			Kind:       string(v1alpha1.RenderTargetConfigMap),
+			APIVersion: "v1",
 		},
 	}
 }
