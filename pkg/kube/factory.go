04b1446574682d392619d9a59bfadae5c0ae5454
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-08-20 14:00:04 +0500

    Fix api version of DaemonSet

--- pkg/kube/factory.go
+++ pkg/kube/factory.go
@@ -16,7 +16,7 @@ var appKindToObjectMap = map[v1alpha1.AppKind]metav1.Object{
 	v1alpha1.AppKindDaemonSet: &v1.DaemonSet{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       string(v1alpha1.AppKindDaemonSet),
-			APIVersion: "apps/v1beta1",
+			APIVersion: "extensions/v1beta1",
 		},
 	},
 	v1alpha1.AppKindStatefulSet: &v1.StatefulSet{
